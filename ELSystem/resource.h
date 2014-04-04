4a7b1c70a9943fbe6cb084db28490f098406d88d
unknown <thatindividual.zhang@gmail.com>
2014-04-04 10:30:04 +0800

    get started

---
+++ ELSystem/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ELSystem.rc
+//
+#define IDC_AXIOM                       1000
+#define IDC_RULE                        1001
+#define IDC_ORDER                       1002
+#define IDC_ANGLE                       1003
+#define IDC_RESET                       1004
+#define IDM_EDIT_CMD                    40002
+#define IDM_APP_ABOUT                   40003
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
